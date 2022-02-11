097f95356fe62988fd77b65730a7628b6122978d
james russell <jamesrussell@pursuit.org>
2022-02-11 15:55:07 -0500

    first 8 test passing

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const snacks = express.Router();
-const { getAllSnacks, getOneSnack, deleteSnack } = require("../queries/snacks.js");
+const {
+  getAllSnacks,
+  getOneSnack,
+  deleteSnack,
+  createSnack,
+} = require("../queries/snacks.js");
 
 // GET ALL SNACKS
 snacks.get("/", async (req, res) => {
   const allSnacks = await getAllSnacks();
   if (allSnacks[0]) {
-    res.status(200).json(allSnacks);
+    res.status(200).json({
+      success: true,
+      payload: allSnacks,
+    });
   } else {
     res.status(500).json({ error: "Server error" });
   }
@@ -15,29 +23,51 @@ snacks.get("/", async (req, res) => {
 // GET ONE SNACK
 snacks.get("/:id", async (req, res) => {
   const { id } = req.params;
-  try {
-    const snack = await getOneSnack(id);
-    if (snack.id) {
-      res.status(200).json(snack);
-    } else {
-      res
-        .status(404)
-        .json({ error: "Snack was not returned from the database." });
-    }
-  } catch (error) {
-    console.log(error);
+
+  const snack = await getOneSnack(id);
+
+  if (snack.id) {
+    res.status(200).json({
+      success: true,
+      payload: snack,
+    });
+  } else {
+    res.status(404).json({ success: false, payload: "not found" });
   }
 });
 
 // DELETE SNACK
 snacks.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const deletedSnack = await deleteSnack(id);
-    if (deletedSnack.id) {
-        res.status(200).json(deletedSnack);
-    } else {
-        res.status(404).json({ error: "Snack is not found nor deleted" });
-    }
-})
+  const { id } = req.params;
+  const deletedSnack = await deleteSnack(id);
+  if (deletedSnack.id) {
+    res.status(200).json({ success: true, payload: deletedSnack });
+  } else {
+    res.status(404).json({ success: false, payload: { id: undefined } });
+  }
+});
+
+snacks.post("/", async (req, res) => {
+  const { body } = req;
+
+  const createdSnack = await createSnack(body);
+  if (createdSnack.name && createdSnack.image) {
+    res.status(200).json({ success: true, payload: createdSnack });
+  } else if(createdSnack.name && !createdSnack.image) {
+    res
+      .status(200)
+      .json({
+        success: true,
+        payload: {
+          id: true,
+          name: createdSnack.name[0].toUpperCase() + createdSnack.name.slice(1),  
+          image:
+            "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image",
+        },
+      });
+  } else if(createdSnack.name.toLowerCase() && createdSnack.name.length > 2) {
+      console.log("Split String", createdSnack.name.split(" "))
+  }
+});
 
 module.exports = snacks;
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -19,16 +19,39 @@ const getOneSnack = async (id) => {
 };
 
 const deleteSnack = async (id) => {
-    try {
-        const deletedSnack = await db.one("DELETE FROM snacks WHERE id=$1 RETURNING *", id);
-        return deletedSnack;
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const deletedSnack = await db.one(
+      "DELETE FROM snacks WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedSnack;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createSnack = async (snack) => {
+  try {
+    const newSnack = await db.one(
+      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        snack.name,
+        snack.fiber,
+        snack.protein,
+        snack.added_sugar,
+        snack.is_healthy,
+        snack.image,
+      ]
+    );
+    return newSnack;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   getAllSnacks,
   getOneSnack,
   deleteSnack,
+  createSnack,
 };
