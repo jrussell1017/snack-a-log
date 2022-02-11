286b3fa76750f0a3980b4e66aebfd1d96e4a225a
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-11 10:42:54 -0500

    completed app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,22 @@
 // DEPENDENCIES
-
+const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
+app.get("/", (req, res)=>{
+    res.send("Get Snack'n at Snack-a-log!");
+})
+
+app.get("*", (req, res)=>{
+    res.status(404).send("Page not found");
+})
 
 // EXPORT
 module.exports = app;
