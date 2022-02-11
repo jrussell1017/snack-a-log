6a3c63f73486849966499b0c431a7291c9a61572
james russell <jamesrussell@pursuit.org>
2022-02-11 11:58:37 -0500

    added get one snack query and get request for one snack

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const snacks = express.Router();
-const { getAllSnacks } = require("../queries/snacks.js");
+const { getAllSnacks, getOneSnack } = require("../queries/snacks.js");
 
-snacks.get("/", async (req, res)=>{
-    const allSnacks = await getAllSnacks();
-    if (allSnacks[0]) {
-        res.status(200).json(allSnacks);
+snacks.get("/", async (req, res) => {
+  const allSnacks = await getAllSnacks();
+  if (allSnacks[0]) {
+    res.status(200).json(allSnacks);
+  } else {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+snacks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const snack = await getOneSnack(id);
+    if (snack.id) {
+      res.status(200).json(snack);
     } else {
-        res.status(500).json({ error: "Server error" });
+      res
+        .status(404)
+        .json({ error: "Snack was not returned from the database." });
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = snacks;
