e888ef43820f49a229ab5076e016b0bf73a3d040
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-12 17:07:36 -0500

    got all 12 tests to pass. To do: confirmHealth.js

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -50,43 +50,82 @@ snacks.delete("/:id", async (req, res) => {
   }
 });
 
+// POST
 snacks.post("/", async (req, res) => {
-  const { id, name, fiber, protein, added_sugar, is_healthy, image } = req.body;
-  const createdSnack = await createSnack(req.body);
-  confirmHealth(createdSnack)
-  if(!createdSnack.image) {
-    res.status(200).json({ success: true, payload: { 
-        id: true,
-        image: "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image" 
-    } 
-  });
-}
+    const { body } = req;
+    const createdSnack = await createSnack(body);
 
-if (deletedSnack.id) {
-    res.status(200).json({ success: true, payload: deletedSnack });
-  } else {
-    res.status(404).json({ success: false, payload: { id: undefined } });
-  }
+    // If there is valid name, but no image
+    if (createdSnack.id && !createdSnack.image) {
+        res.status(200).json({
+            success: true,
+            payload: {
+                id: createdSnack.id,
+                name: spidersOnALog(createdSnack.name),
+                fiber: createdSnack.fiber,
+                protein: createdSnack.protein,
+                added_sugar: createdSnack.added_sugar,
+                is_healthy: createdSnack.is_healthy,
+                image: "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image"
+            }
+        })
+    } else if (createdSnack.id) {
+        // Can create a snack
+        res.status(200).json({
+            success: true,
+            payload: {
+                id: createdSnack.id,
+                name: spidersOnALog(createdSnack.name),
+                fiber: createdSnack.fiber,
+                protein: createdSnack.protein,
+                added_sugar: createdSnack.added_sugar,
+                is_healthy: createdSnack.is_healthy,
+                image: createdSnack.image
+            }
+        })
+    } else {
+        res.status(500).json({ error: "Snack creation error" })
+    }
+})
 
-  if (createdSnack.id) {
-    res.status(200).json({ 
-        success: true, 
-        payload: {
-            id: id,
-            name: spidersOnALog(name),
-            fiber: fiber,
-            protein: protein,
-            added_sugar: added_sugar,
-            is_healthy: is_healthy,
-            image: image
-        }
-        });
-  } else {
-    res.status(400).json({
-        success: false, 
-        payload: { is_healthy: undefined }
-    })
-  }
-});
+
+// snacks.post("/", async (req, res) => {
+//   const { id, name, fiber, protein, added_sugar, is_healthy, image } = req.body;
+//   const createdSnack = await createSnack(req.body);
+//   confirmHealth(createdSnack)
+//   if(!createdSnack.image) {
+//     res.status(200).json({ success: true, payload: { 
+//         id: true,
+//         image: "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image" 
+//     } 
+//   });
+// }
+
+// if (deletedSnack.id) {
+//     res.status(200).json({ success: true, payload: deletedSnack });
+//   } else {
+//     res.status(404).json({ success: false, payload: { id: undefined } });
+//   }
+
+//   if (createdSnack.id) {
+//     res.status(200).json({ 
+//         success: true, 
+//         payload: {
+//             id: id,
+//             name: spidersOnALog(name),
+//             fiber: fiber,
+//             protein: protein,
+//             added_sugar: added_sugar,
+//             is_healthy: is_healthy,
+//             image: image
+//         }
+//         });
+//   } else {
+//     res.status(400).json({
+//         success: false, 
+//         payload: { is_healthy: undefined }
+//     })
+//   }
+// });
 
 module.exports = snacks;
