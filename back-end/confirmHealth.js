36769dc7b252a543fd94699f400c23d73750841a
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-14 13:28:14 -0500

    confirmHealth.js is now a function. Passing all tests

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,5 +1,30 @@
-function confirmHealth(snack) {
-    let { fiber, protein, added_sugar, is_healthy } = snack;
+const confirmHealth = (snack) => {
+    if (typeof snack.protein !== "number" && typeof snack.fiber !== "number" && typeof snack.added_sugar !== "number" ) {
+        return null;
+    }
+
+    if (snack.fiber >= 5 && snack.added_sugar < 5) {
+        return true;
+    } else if (snack.protein >= 5 && snack.added_sugar < 5) { 
+        return true;
+    }
+    return false;
+};
+
+module.exports = confirmHealth;
+
+
+// const { body } = req;
+// console.log("healthy:", body.is_healthy, "fiber:", body.fiber, "sugar:", body.added_sugar)
+
+// if (body.fiber >= 5 && body.added_sugar < 5) {
+//     body.is_healthy = true;
+//     next();
+// } else {
+//     res.status(400).json({ error: "Fiber is not ok" });
+// }
+
+    // let { fiber, protein, added_sugar, is_healthy } = snack;
     // console.log(snack);
     // console.log(is_healthy);
 
@@ -8,13 +33,13 @@ function confirmHealth(snack) {
     //     return null;
     // }
 
-    if (fiber >= 5 && added_sugar < 5) {
-        is_healthy = true;
-    } else {
-        is_healthy = false;
-    }
-    // console.log(is_healthy);
-    return is_healthy
+    // if (fiber >= 5 && added_sugar < 5) {
+    //     is_healthy = true;
+    // } else {
+    //     is_healthy = false;
+    // }
+    // // console.log(is_healthy);
+    // return is_healthy
 
     // if (protein > 5 && added_sugar < 5) {}
     // if (protein > 5 || fiber > 5 && sugar < 5) {}
@@ -22,10 +47,10 @@ function confirmHealth(snack) {
     // if (protein > 5 && added_sugar > 5) {}
     // if (protein > 5 && fiber > 5 && added_sugar > 5) {}  
     // if (typeof protein === null || typeof fiber === null  || typeof added_sugar === null ) {}
-};    
+// };    
 
 // *** #1 *** it("Checks if fiber is above five and added_sugar is below 5"
-console.log(confirmHealth({ protein: 4, fiber: 5, added_sugar: 1 })) //true
+// console.log(confirmHealth({ protein: 4, fiber: 5, added_sugar: 1 })) //true
 
 // *** #2 ***     it("Checks if protein is above 5 and added_sugar is below 5"
 // confirmHealth({ protein: 6, fiber: 2, added_sugar: 0 }) //   true
@@ -47,4 +72,4 @@ console.log(confirmHealth({ protein: 4, fiber: 5, added_sugar: 1 })) //true
 // // *** #7 *** "Checks if protein, fiber and added_sugar have valid values"
 // confirmHealth({ protein: "", fiber: "c", added_sugar: null }) //null
  
-module.exports = { confirmHealth };
+
