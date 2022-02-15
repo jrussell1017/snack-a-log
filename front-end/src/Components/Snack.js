3544c8d81e10013cddfa23af540e893132008d37
james russell <jamesrussell@pursuit.org>
2022-02-15 16:59:01 -0500

    tested card component

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -1,9 +1,13 @@
 function Snack({ snack }) {
   return (
-    <div>
-      <img src={snack.image} alt="snack"></img>
-      {snack.name}
-      Healthy: {snack.is_healthy}
+    <div class="card">
+      <img src={snack.image} alt="image" style="width:100%" />
+      <div class="container">
+        <h4>
+          <b>{snack.name}</b>
+        </h4>
+        {snack.is_healthy}
+      </div>
     </div>
   );
 }
