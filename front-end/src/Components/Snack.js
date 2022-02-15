687cc6fd8ecd8a74909b53914a4761614d1528d2
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-15 16:07:21 -0500

    Added card and CSS. Passing all index.js tests.

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -1,9 +1,20 @@
-function Snack({ snack }) {
+import { Link } from "react-router-dom";
+import HeartHealth from "./HeartHealth";
+
+function Snack({ snack, id }) {
   return (
-    <div>
-      <img src={snack.image} alt="snack"></img>
-      {snack.name}
-      Healthy: {snack.is_healthy}
+    <div className="Snack">
+      <a href={`/snacks/${id}`}>
+        <h4>
+          {snack.is_healthy ? (
+            <img src={snack.image} alt="healthy food"></img>
+          ) : (
+            <img src={snack.image} alt="unhealthy food"></img>
+          )}
+          <HeartHealth snackHealth={snack.is_healthy} />
+          {snack.name}
+        </h4>
+      </a>
     </div>
   );
 }
